b818a1b5521823a333c265bf2ecf1ff98d68a724
Casey Watts <casey.s.watts@gmail.com>
2024-09-14 13:39:35 -0400

    basic Astro site, rendering exercises

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({});
+const exercises = defineCollection({});
+export const collections = { exercises };
