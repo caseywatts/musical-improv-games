5d64a9ce1feb460c608d317a3a3048d1426f61f9
Casey Watts <casey.s.watts@gmail.com>
2024-09-14 14:19:31 -0400

    define collections in the right place; add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
-import { defineCollection } from "astro:content";
 
 // https://astro.build/config
 export default defineConfig({});
-const exercises = defineCollection({});
-export const collections = { exercises };
---
+++ src/content/config.ts
@@ -0,0 +1,5 @@
+import { defineCollection } from "astro:content";
+
+// https://astro.build/config
+const exercises = defineCollection({});
+export const collections = { exercises };
